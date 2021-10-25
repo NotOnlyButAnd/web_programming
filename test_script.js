9ae5df3dfd436c2577e885c569e726b2d6d44797
Kirill <72963968+NotOnlyButAnd@users.noreply.github.com>
2021-10-25 18:07:55 +0300

    First test script (JS)
    Первый скрипт на JS. Примитивненько))

---
+++ test_script.js
@@ -0,0 +1,7 @@
+'use strict'; // Вход в строгий режим, пока что желателен
+// alert("Hello world, Java script!")   // вывод уведомления (браузерная фишка) 
+let message1 = "Hello world...";    // переменная
+const language = "JavaScript!!!";   // константа
+const COLOR_ORANGE = "#FF7F00";     // константа-псевдоним тип
+alert(message1);
+alert(language);
